d1a8300fcdb5f9c3ecd57ed338b599cf0393432f
Tanner Dale <tanner@tannerdale.com>
2021-09-03 23:03:24 -0500

    Add validation for uniq pets per application

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -2,5 +2,7 @@ class ApplicationPet < ApplicationRecord
   belongs_to :application
   belongs_to :pet
 
+  validates_uniqueness_of :pet_id, scope: :application_id
+
   enum status: %w(pending approved rejected)
 end
