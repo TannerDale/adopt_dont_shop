a807ee20e62fb95b128461405ac9048aeace866e
Tanner Dale <tanner@tannerdale.com>
2021-09-09 12:22:10 -0500

    Abstract fox_picture api call to fox_picture_service

--- app/facades/fox_picture_facade.rb
+++ app/facades/fox_picture_facade.rb
@@ -1,15 +1,8 @@
 class FoxPictureFacade
   class << self
     def get_a_picture
-      FoxPicture.picture(parse)
-    end
-
-    def response
-      Faraday.get("https://randomfox.ca/floof/")
-    end
-
-    def parse
-      JSON.parse(response.body, symbolize_names: true)
+      data = FoxPictureService.parse
+      FoxPicture.picture(data)
     end
   end
 end
